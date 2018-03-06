fc8f6b687b6ee5d896c5ad8c0e51a5f281875980
Shikhar Sharma <shikhar.sharma@microsoft.com>
2018-03-06 18:56:42 +0000

    fixes #11 : add functionality for compute_metrics to accept list of lists apart from files

--- test/api.py
+++ test/api.py
@@ -3,11 +3,19 @@ from nlgeval import NLGEval
 def test_oo_api():
     with open("examples/hyp.txt") as f:
         hyp = f.readlines()
+        hyp = [x.strip() for x in hyp]
     with open("examples/ref1.txt") as f:
         ref1 = f.readlines()
+        ref1 = [x.strip() for x in ref1]
     with open("examples/ref2.txt") as f:
         ref2 = f.readlines()
+        ref2 = [x.strip() for x in ref2]
 
     nlge = NLGEval()
-    res = nlge.evaluate([ref1[0]] + [ref2[0]], hyp[0])
-    res = nlge.evaluate([ref1[1]] + [ref2[1]], hyp[1])
+
+    res = nlge.evaluate_single([ref1[0]] + [ref2[0]], hyp[0])
+    res = nlge.evaluate_single([ref1[1]] + [ref2[1]], hyp[1])
+
+    hyp_list = hyp
+    ref_list = [ref1, ref2]
+    res = nlge.evaluate_corpus(ref_list, hyp_list)
